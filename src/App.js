916ff9ddcbb325890b2ea25d301bc7e3546d1d3d
selvaramj <jselvamanu123@gmail.com>
2020-11-07 09:58:04 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Button,InputTypes} from 'react-foundation';
+import 'foundation-sites';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App  ">
      <Button className="buttonStyle">Welcome</Button>
-     
+     <div><button></button></div>
 
     </div>
   );
